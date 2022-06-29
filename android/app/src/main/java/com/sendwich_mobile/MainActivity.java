67d770e4f2a924f82e20a45a9f1ff5b1d2175eeb
Bagoomi <bwj2517@naver.com>
2022-06-29 22:42:35 +0900

    안드로이드적용 브레이크포인트, 키해시등록

--- android/app/src/main/java/com/sendwich_mobile/MainActivity.java
+++ android/app/src/main/java/com/sendwich_mobile/MainActivity.java
@@ -3,8 +3,46 @@ package com.sendwich_mobile;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+
+import android.util.Base64;
+import android.util.Log;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle saveInstanceState){
+    super.onCreate(saveInstanceState);
+    //setContentView(R.layout.activity_main);
+
+    getHashKey();
+  }
+
+  private void getHashKey(){
+    PackageInfo packageInfo = null;
+    try {
+      packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+    } catch (PackageManager.NameNotFoundException e) {
+      e.printStackTrace();
+    }
+    if (packageInfo == null)
+      Log.e("KeyHash", "KeyHash:null");
+
+    for (Signature signature : packageInfo.signatures) {
+      try {
+        MessageDigest md = MessageDigest.getInstance("SHA");
+        md.update(signature.toByteArray());
+        Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+      } catch (NoSuchAlgorithmException e) {
+        Log.e("KeyHash", "Unable to get MessageDigest. signature=" + signature, e);
+      }
+    }
+  }
+
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
