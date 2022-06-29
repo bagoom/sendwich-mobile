07b95288f5277494e2af2462beb30e67cf712329
Bagoomi <bwj2517@naver.com>
2022-06-29 20:36:05 +0900

    IOS 빌드 성공 브레이크 포인트

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import * as React from 'react';
 import App from './routes/root';
 import globalStore, {GlobalStore} from './store/store';
@@ -6,11 +5,9 @@ import {StoreProvider} from './store/util';
 
 function Main() {
   return (
-    <GestureHandlerRootView style={{flex: 1}}>
-      <StoreProvider store={globalStore as GlobalStore}>
-        <App />
-      </StoreProvider>
-    </GestureHandlerRootView>
+    <StoreProvider store={globalStore as GlobalStore}>
+      <App />
+    </StoreProvider>
   );
 }
 
